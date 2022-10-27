351f5b0eb983b2114166495cb425aafc22fe9918
sreenivasEdvoy <sreenivas@edvoy.com>
2022-10-28 00:01:27 +0530

    Automate mail send to sunil website

--- s.py
+++ s.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-
 import json
 f = open('q.json')
 data = json.load(f)
@@ -16,7 +15,6 @@ driver.maximize_window()
 
 driver.get('https://sunilreddy-chinthaparthi.github.io/Personal_Website/')
 wait = WebDriverWait(driver, 10)
-# wait.until(ec.visibility_of_element_located((By.ID, "contact")))
 def clear():
     driver.find_element(By.XPATH, '//*[@id="contact"]/div/form/input[1]').clear()
     driver.find_element(By.XPATH, '//*[@id="contact"]/div/form/input[2]').clear()
@@ -27,16 +25,12 @@ for i in data['quotes']:
     driver.find_element(By.XPATH, '//*[@id="contact"]/div/form/input[1]').send_keys(author)
     driver.find_element(By.XPATH, '//*[@id="contact"]/div/form/input[2]').send_keys(author+'@Gmail.com')
     driver.find_element(By.XPATH, '//*[@id="contact"]/div/form/textarea').send_keys(quote)
-    # driver.find_element_by_class_name('btn btn-primary').click();
     time.sleep(2)
     element = driver.find_element(By.XPATH, '//*[@id="contact"]/div/form/button')
     driver.execute_script("arguments[0].click();", element)
-
-   
     print("Send -"+str(i))
-    time.sleep(10)
+    time.sleep(1)
     clear()
-
 driver.close()
 
 
